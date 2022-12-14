abab41ea23f0a4851729e454a33f3135daec8e6b
Johan Sanchez <jsanchez@opcionauto.com>
2022-12-14 17:17:10 -0500

    :sparkles: feat: websockets
    Agrega y configura websockets en proyecto de laravel
    
    BREAKING CHANGE: First Commit

---
+++ app/Events/TestEvent.php
@@ -0,0 +1,41 @@
+<?php
+
+namespace App\Events;
+
+use Illuminate\Broadcasting\Channel;
+use Illuminate\Broadcasting\InteractsWithSockets;
+use Illuminate\Broadcasting\PresenceChannel;
+use Illuminate\Broadcasting\PrivateChannel;
+use Illuminate\Contracts\Broadcasting\ShouldBroadcast;
+use Illuminate\Foundation\Events\Dispatchable;
+use Illuminate\Queue\SerializesModels;
+
+class TestEvent implements ShouldBroadcast
+{
+    use Dispatchable, InteractsWithSockets, SerializesModels;
+
+    /**
+     * Create a new event instance.
+     *
+     * @return void
+     */
+    public function __construct()
+    {
+        //
+    }
+
+    public function broadcastOn(): Channel
+    {
+        return new Channel('events');
+    }
+
+    public function broadcastAs(): string
+    {
+        return 'testing';
+    }
+
+    public function broadcastWith()
+    {
+        return ['msg' => 'Hola Mundo'];
+    }
+}
